2a43622923f62410d6334afcb0c2cb90bcacd178
jose.junior <josejuniorwm@gmail.com>
2024-09-04 20:35:21 -0300

    Tipo

--- app.js
+++ app.js
@@ -40,6 +40,25 @@ class Bd {
 
 		localStorage.setItem('id', id)
 	}
+
+	recuperarTodosRegistros(){
+
+		let despesas = Array()
+
+		let id = localStorage.getItem('id')
+
+		for(let i = 1; i <= id ; i++) {
+			let despesa = JSON.parse(localStorage.getItem(i))
+
+			if(despesa === null){
+				continue
+			}
+
+			despesas.push(despesa)
+		}
+
+		return despesas
+	}
 }
 
 let bd = new Bd()
@@ -86,4 +105,50 @@ function cadastrarDespesa() {
 		//dialog de erro
 		$('#modalRegistraDespesa').modal('show')
 	}
+}
+
+function carregaListaDespesas() {
+
+	let despesas = Array()
+
+	despesas = bd.recuperarTodosRegistros()
+
+	var listaDespesa = document.getElementById('listaDespesa')
+
+	/*
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	*/
+
+	despesas.forEach(function (d) {
+
+		let linha = listaDespesas.insertRow()
+
+		linha.insertCell(0).innerHTML = d.dia + '/' + d.mes + '/' + d.ano
+
+		switch (d.tipo) {
+			case '1': d.tipo = 'Ali'
+				break;
+			case '2': d.tipo = 'Ed'
+				break;
+			case '3': d.tipo = 'La'
+				break;
+			case '4': d.tipo = 'Sa'
+				break;
+			case '5': d.tipo = 'Tra'
+				break;
+			default:
+				break;
+		}
+
+		linha.insertCell(1).innerHTML = d.tipo
+
+		linha.insertCell(2).innerHTML = d.descricao
+		linha.insertCell(3).innerHTML = d.valor
+
+	})
 }
